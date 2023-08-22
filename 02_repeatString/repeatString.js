975e8207a3433f8a05a1ace3dd4ee6d9cc118cb1
David <david.nguyen00@outlook.com>
2023-08-22 13:58:02 -0700

    Solved first four exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let message = ''
+    if (num < 0) {
+        return 'ERROR'
+    }
+    for (let i = 0; i < num; i++) {
+        message += string;
+    }
+    return message;
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...manyMoreArgs) {
+    const filtered = array.filter(element => !manyMoreArgs.includes(element));
+    return filtered;
 };
 
 // Do not edit below this line
